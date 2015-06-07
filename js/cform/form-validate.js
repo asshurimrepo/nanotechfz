91a181a16616b180a1c68fcd666770a0bb25e922
Admin <powerlogic1992@gmail.com>
2015-06-07 18:54:34 +0800

    first commit.

--- js/cform/form-validate.js
+++ js/cform/form-validate.js
@@ -1,17 +1,17 @@
 function valid_datas( f ){
-
+	
 	if( f.name.value == '' ){
 		jQuery('#form_status').html('<span class="wrong">Your name must not be empty!</span>');
 		notice( f.name );
 	}else if( f.email.value == '' ){
 		jQuery('#form_status').html('<span class="wrong">Your email must not be empty and correct format!</span>');
 		notice( f.email );
-	}else if( f.phone.value == '' ){
-		jQuery('#form_status').html('<span class="wrong">Your phone must not be empty and correct format!</span>');
-		notice( f.phone );
-	}else if( f.subject.value == '' ){
+	//}else if( f.phone.value == '' ){
+		//jQuery('#form_status').html('<span class="wrong">Your phone must not be empty and correct format!</span>');
+		//notice( f.phone );
+	/*}else if( f.subject.value == '' ){
 		jQuery('#form_status').html('<span class="wrong">Your subject must not be empty!</span>');
-		notice( f.subject );
+		notice( f.subject );*/
 	}else if( f.message.value == '' ){
 		jQuery('#form_status').html('<span class="wrong">Your message must not be empty!</span>');
 		notice( f.message );
